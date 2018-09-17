40f271ff82167a51e7a05b6556cc60d8e454612e
geoffwlfetch <geoffwfletch@gmail.com>
2018-09-17 20:48:31 +1000

    .

---
+++ TestApp/Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TestApp
+{
+    [TestFixture]
+    class Test
+    {
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        [TestCase]
+        public void Test1()
+        {
+        }
+    }
+}
